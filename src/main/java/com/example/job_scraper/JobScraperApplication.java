9a69694759c45e89567db94d713fc5ca46bf1ab6
Kukharenko Yurii <46027349+yurakukh@users.noreply.github.com>
2026-04-28 23:05:58 +0300

    Init commit

---
+++ src/main/java/com/example/job_scraper/JobScraperApplication.java
@@ -0,0 +1,13 @@
+package com.example.job_scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/job_scraper/JobScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job_scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
